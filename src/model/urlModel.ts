17ce50e2fe09521b7d0e7b9081bfce08d511887d
michaelc511 <michaelc511@gmail.com>
2024-03-05 11:12:32 -0800

    added protocol to short URL

--- src/model/urlModel.ts
+++ src/model/urlModel.ts
@@ -18,4 +18,14 @@ const urlSchema: Schema = new Schema({
     } 
 });
 
+urlSchema.pre<IUrl>('save', function(next) {
+    const fullUrl = this.full;
+    const protocolIndex = fullUrl.indexOf('://');
+    if (protocolIndex !== -1) {
+        const protocol = fullUrl.substring(0, protocolIndex + 3);
+        this.short = protocol + this.short;
+    }
+    next();
+});
+
 export default mongoose.model<IUrl>('Url', urlSchema);
